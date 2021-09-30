d134fbc234c9ef69386d9aefdae047c2be816802
MdEhsan <mdehsan57@gmail.com>
2021-09-30 21:13:55 +0530

    code updated and error fixed

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
-const firstInput = parseInt(document.querySelector('.first-input').value);
-const secndInput = parseInt(document.querySelector('.secnd-input').value);
+const firstInput = document.querySelector('.first-input');
+const secndInput = document.querySelector('.secnd-input');
 const addBtn = document.querySelector('.add-btn');
 const subBtn = document.querySelector('.sub-btn');
 const multiBtn = document.querySelector('.multi-btn');
 const divBtn = document.querySelector('.div-btn');
-// const outPut = parseInt(document.querySelector('.result').value);
+const outPut = document.querySelector('.result');
 
-
+// Addition Function
 
 function add(){
-    document.querySelector('.result').value = firstInput + secndInput
+    outPut.value = parseInt(firstInput.value) + parseInt(secndInput.value)
 }
 
-
-
 addBtn.addEventListener('click', add);
+
+
+// Subtraction function
+
+function sub(){
+    outPut.value = parseInt(firstInput.value) - parseInt(secndInput.value)
+}
+
+subBtn.addEventListener('click' , sub);
+
+// Multiplication Function
+
+function multi(){
+    outPut.value = parseInt(firstInput.value) * parseInt(secndInput.value)
+}
+
+multiBtn.addEventListener('click' , multi);
+
+// Division Function
+
+function divide(){
+    outPut.value = parseInt(firstInput.value) / parseInt(secndInput.value)
+}
+
+divBtn.addEventListener('click' , divide);
+
 
